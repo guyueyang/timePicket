d9eede118d4823914c3fa19f03a7d59a4c50e162
yangxioaping <yangxiaoping@westcoast.com.cn>
2017-02-28 16:11:14 +0800

    时间控件

---
+++ timepicket/timepicket/src/main/java/com/guyueyang/timePicket/OnWheelScrollListener.java
@@ -0,0 +1,7 @@
+package com.guyueyang.timePicket;
+
+public interface OnWheelScrollListener {
+	void onScrollingStarted(WheelView wheel);
+
+	void onScrollingFinished(WheelView wheel);
+}
---
+++ timepicket/timepicket/src/main/java/com/guyueyang/timePicket/WheelAdapter.java
@@ -0,0 +1,10 @@
+
+package com.guyueyang.timePicket;
+
+public interface WheelAdapter {
+	public int getItemsCount();
+
+	public String getItem(int index);
+
+	public int getMaximumLength();
+}
